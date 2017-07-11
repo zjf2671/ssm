625a89dad0534b4460f581d8906253d8e17e76e7
harry.zhang <zjf2671@126.com>
2017-07-11 19:09:48 +0800

    增加个推消息推送

---
+++ src/main/java/com/ssm/common/getui/PushtoSingle.java
@@ -0,0 +1,80 @@
+package com.ssm.common.getui;
+import com.gexin.rp.sdk.base.IPushResult;
+import com.gexin.rp.sdk.base.impl.SingleMessage;
+import com.gexin.rp.sdk.base.impl.Target;
+import com.gexin.rp.sdk.exceptions.RequestException;
+import com.gexin.rp.sdk.http.IGtPush;
+import com.gexin.rp.sdk.template.LinkTemplate;
+import com.gexin.rp.sdk.template.style.Style0;
+
+/**
+ * @Description: 单个机器推送 
+ * @author harry.zhang
+ * @CreateDate:	2017年7月11日
+ * @version 1.0
+ */
+public class PushtoSingle {
+    //采用"Java SDK 快速入门"， "第二步 获取访问凭证 "中获得的应用配置，用户可以自行替换
+    private static String appId = "qs7TAAijc78EYeZr2nbZZ3";
+    private static String appKey = "4tYioFVKcT7xJdgold5iT2";
+    private static String masterSecret = "hZcEq5a57U8pAEySOcWpR8";
+    
+
+    static String CID = "5abfe84bf25dfc9d5fcbd69126a33423";
+   //别名推送方式
+   // static String Alias = "";
+    static String host = "http://sdk.open.api.igexin.com/apiex.htm";
+
+    public static void main(String[] args) throws Exception {
+        IGtPush push = new IGtPush(host, appKey, masterSecret);
+        LinkTemplate template = linkTemplateDemo();
+        template.setUrl("http://www.baidu.com");
+        SingleMessage message = new SingleMessage();
+        message.setOffline(true);
+        // 离线有效时间，单位为毫秒，可选
+        message.setOfflineExpireTime(24 * 3600 * 1000);
+        message.setData(template);
+        // 可选，1为wifi，0为不限制网络环境。根据手机处于的网络情况，决定是否下发
+        message.setPushNetWorkType(0); 
+        Target target = new Target();
+        target.setAppId(appId);
+        target.setClientId(CID);
+        //target.setAlias(Alias);
+        IPushResult ret = null;
+        try {
+            ret = push.pushMessageToSingle(message, target);
+        } catch (RequestException e) {
+            e.printStackTrace();
+            ret = push.pushMessageToSingle(message, target, e.getRequestId());
+        }
+        if (ret != null) {
+            System.out.println(ret.getResponse().toString());
+        } else {
+            System.out.println("服务器响应异常");
+        }
+    }
+    public static LinkTemplate linkTemplateDemo() {
+        LinkTemplate template = new LinkTemplate();
+        // 设置APPID与APPKEY
+        template.setAppId(appId);
+        template.setAppkey(appKey);
+
+        Style0 style = new Style0();
+        // 设置通知栏标题与内容
+        style.setTitle("这个是单独推送的");
+        style.setText("这是一单独条推送消息~");
+        // 配置通知栏图标
+        style.setLogo("icon.png");
+        // 配置通知栏网络图标
+        style.setLogoUrl("");
+        // 设置通知是否响铃，震动，或者可清除
+        style.setRing(true);
+        style.setVibrate(true);
+        style.setClearable(true);
+        template.setStyle(style);
+
+        // 设置打开的网址地址
+        template.setUrl("http://www.baidu.com");
+        return template;
+    }
+}

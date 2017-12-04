358b557146376a50f38e9e124284431a77723577
harry.zhang <zjf2671@126.com>
2017-12-04 19:18:04 +0800

    增加rabbitmq

--- src/main/java/com/ssm/service/rabbitmq/impl/QueueListenter.java
+++ src/main/java/com/ssm/service/rabbitmq/impl/QueueListenter.java
@@ -8,6 +8,12 @@ import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.stereotype.Service;
 
+/**
+ * @Description: 功能概要：消息获取  
+ * @author harry.zhang
+ * @CreateDate:	2017年12月4日
+ * @version 1.0
+ */
 @Service
 public class QueueListenter implements MessageListener{
 	
